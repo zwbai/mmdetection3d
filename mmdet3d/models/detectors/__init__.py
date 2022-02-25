a2153a0b82349d7695ebadcf608a475483cede71
zwbai <zwbai@bjtu.edu.cn>
2022-02-24 23:37:50 -0800

    update pillargrid with separate encoder

--- mmdet3d/models/detectors/__init__.py
+++ mmdet3d/models/detectors/__init__.py
@@ -15,10 +15,11 @@ from .ssd3dnet import SSD3DNet
 from .votenet import VoteNet
 from .voxelnet import VoxelNet
 from .pillargrid import PillarGrid
+from .pillargrid import PillarGridShare
 
 __all__ = [
     'Base3DDetector', 'VoxelNet', 'DynamicVoxelNet', 'MVXTwoStageDetector',
     'DynamicMVXFasterRCNN', 'MVXFasterRCNN', 'PartA2', 'VoteNet', 'H3DNet',
     'CenterPoint', 'SSD3DNet', 'ImVoteNet', 'SingleStageMono3DDetector',
-    'FCOSMono3D', 'ImVoxelNet', 'GroupFree3DNet', 'PillarGrid'
+    'FCOSMono3D', 'ImVoxelNet', 'GroupFree3DNet', 'PillarGrid','PillarGridShare'
 ]
