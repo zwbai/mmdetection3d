83f9638ba8ed2bd71fb8384513a7e36ba9b82f83
zwbai <zwbai@bjtu.edu.cn>
2022-04-22 21:45:49 -0700

    update

--- mmdet3d/models/voxel_encoders/__init__.py
+++ mmdet3d/models/voxel_encoders/__init__.py
@@ -1,8 +1,9 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .pillar_encoder import PillarFeatureNet
+from .attention_encoder import PointAttentionNet
 from .voxel_encoder import DynamicSimpleVFE, DynamicVFE, HardSimpleVFE, HardVFE
 
 __all__ = [
     'PillarFeatureNet', 'HardVFE', 'DynamicVFE', 'HardSimpleVFE',
-    'DynamicSimpleVFE'
+    'DynamicSimpleVFE', 'PointAttentionNet'
 ]
